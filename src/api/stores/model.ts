5e56abf837cc77ec78b750d20bd170bfe8ba81e6
Zaide Kurti <zaiido1920@gmail.com>
2023-05-18 13:50:01 +0200

    Update model.ts

--- src/api/stores/model.ts
+++ src/api/stores/model.ts
@@ -4,6 +4,7 @@ const { Schema, model } = mongoose;
 
 const storeSchema = new Schema(
     {
+        name: { type: String, required: true },
         coordinates: [{ type: Number, required: true }],
         address: {
             street: String,
@@ -12,7 +13,7 @@ const storeSchema = new Schema(
             city: String,
             country: String
         },
-        approved: { type: Boolean }
+        approved: { type: Boolean, default: false }
 
     },
     { timestamps: true }
