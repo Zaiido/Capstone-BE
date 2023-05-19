50c61a630a54f57e3a8d429fd3541af7a3c131f0
Zaide Kurti <zaiido1920@gmail.com>
2023-05-19 15:21:23 +0200

    Update index.ts

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -44,6 +44,8 @@ usersRouter.get(
     passport.authenticate("facebook", { session: false }),
     (request: any, response: Response, next: NextFunction) => {
         try {
+            response.setHeader("Access-Control-Allow-Origin", "https://capstone-zaiido.vercel.app");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
             response.cookie("accessToken", request.user!.accessToken, { sameSite: "none", secure: true });
             response.redirect(`${process.env.FE_URL}`);
         } catch (error) {
